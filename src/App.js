2d95257440ebeebe74c4d051d5982f8d7dd62d3d
awais512 <raza.awais25@gmail.com>
2022-07-16 10:49:31 +0500

    Routing and Folder Structures

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import { Actors, MovieInfo, Movies, NavBar, Profile } from "./components";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CssBaseline />
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/movie/:id" element={<MovieInfo />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
